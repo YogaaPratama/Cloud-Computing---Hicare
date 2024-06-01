d0ec5a28801e2ccb7ac10a3a7873addba06f8bed
YogaaPratama <117842022+YogaaPratama@users.noreply.github.com>
2024-06-01 11:59:09 +0700

    Delete services directory

--- services/loadModel.js
+++
@@ -1,8 +0,0 @@
-const tf = require('@tensorflow/tfjs-node');
-
-async function loadModel() {
-  const model = await tf.loadGraphModel(process.env.MODEL_URL);
-  return model;
-}
-
-module.exports = loadModel;
